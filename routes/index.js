b33a33bd0da99766a1a1f2a3ac32754a17d5fc9a
solomon <solomonghebrelibanos@gmail.com>
2021-04-18 13:53:54 -0700

    done 1

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res)=>{
-    res.send('Hello Solomon your first App')
+    res.send('Hello Solomon your first App, Please don not giv up')
 })
 
 module.exports = router
